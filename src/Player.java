d6763f8e2b50400472dc8fe62f39b1927b439607
Xenipulator <apo019@latech.edu>
2020-08-03 00:00:40 -0400

    A

--- src/Player.java
+++ src/Player.java
@@ -3,8 +3,15 @@ import java.util.*;
 public class Player 
 {
     private final String playerName;
-    private String currentRoom;
+    private int currentRoom = 1;
     //need a list of items in inventory
+
+    //constructor
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+
 
 
 }
